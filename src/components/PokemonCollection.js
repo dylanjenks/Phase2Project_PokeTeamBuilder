fb91f1fcab9aab3618c26b3bee0066ef0fac2a95
dylanjenks <dylan.jenks92@gmail.com>
2021-10-26 16:29:45 -0400

    Template upload

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,13 +2,12 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection({allPokemon}) {
-  return (
-    <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
-      {allPokemon.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon}/>)}
-    </Card.Group>
-  );
+function PokemonCollection({ pokemon }) {
+  const cards = pokemon.map((poke) => (
+    <PokemonCard key={poke.id} pokemon={poke} />
+  ));
+
+  return <Card.Group itemsPerRow={6}>{cards}</Card.Group>;
 }
 
 export default PokemonCollection;
